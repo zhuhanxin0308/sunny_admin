c6f43366ea51351542a9986dd7b8d347475f0cad
zhuhanxin0308 <zhuhanxin0308@163.com>
2018-09-14 19:12:49 +0800

    优化了ip转地址代码

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -4,17 +4,22 @@ use think\Controller;
 class Common extends Controller{
 	//自制ip转地名接口
     public function getcity($ip=""){
+	    if($ip==""){
+		    
+return json(array('code'=>400,'msg'=>'缺少参数','data'=>[]));
+	    }
         //所有省份
-        $city=[
-            '北京','天津','上海','重庆','河北','山西','辽宁','吉林','黑龙江','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','海南','四川','贵州','云南','陕西','甘肃','青海','台湾','内蒙古','广西','西藏','宁夏','新疆','香港','澳门'
-        ];
-        $res=file_get_contents("https://baidu.com/s?wd=".$ip);
-        $t=substr($res, 153000,1000);
-        foreach ($city as $key => $value) {
-           if(strpos($t, $value)){
-                return $value;
-           }
-        }
-        return "未知";
+   $city=[
+      '北京','天津','上海','重庆','河北','山西','辽宁','吉林','黑龙江','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','海南','四川','贵州','云南','陕西','甘肃','青海','台湾','内蒙古','广西','西藏','宁夏','新疆','香港','澳门'
+  ];
+    $test=file_get_contents('http://www.baidu.com/s?wd='.$ip);
+    $ex="/<div class=\"result-op c-container\".*?>.*?<\/div>/s";
+    preg_match_all($ex,$test,$res);
+    foreach ($city as $key => $value) {
+      if(strpos($res[0][0], $value)){
+          return json(array('code'=>200,'msg'=>'查询成功','data'=>$value));
+      }
+   }
+   return json(array('code'=>200,'msg'=>'查询成功','data'=>"非大陆"));
     }
 }
